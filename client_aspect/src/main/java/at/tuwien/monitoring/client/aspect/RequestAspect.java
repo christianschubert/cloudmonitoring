4b948d7408ce8521c6363719093aa91d22d0adcb
Christian <Christian@DESKTOP-MVT9AEK>
2017-03-31 13:27:07 +0200

    jms to aspect

--- client_aspect/src/main/java/at/tuwien/monitoring/client/aspect/RequestAspect.java
+++ client_aspect/src/main/java/at/tuwien/monitoring/client/aspect/RequestAspect.java
@@ -1,17 +1,41 @@
 package at.tuwien.monitoring.client.aspect;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.annotation.Annotation;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import at.tuwien.monitoring.client.aspect.jms.JmsService;
+import at.tuwien.monitoring.client.constants.Constants;
+
 @Aspect
 public class RequestAspect {
+
+	private final static Logger logger = Logger.getLogger(RequestAspect.class);
+
+	private JmsService jmsService;
+
+	public RequestAspect() {
+		String publicIPAddress = lookupPublicIPAddress();
+		logger.info("Public IP address of client: " + publicIPAddress);
+		jmsService = new JmsService(Constants.brokerURL, publicIPAddress);
+		jmsService.start();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		jmsService.stop();
+		super.finalize();
+	}
 
 	@Pointcut("@annotation(MonitorRequest)")
 	public void hasMonitorRequestAnnotation() {
@@ -76,5 +100,26 @@ public class RequestAspect {
 		}
 
 		return response;
+	}
+
+	private String lookupPublicIPAddress() {
+		BufferedReader reader = null;
+		try {
+			URL checkIP = new URL("http://checkip.amazonaws.com");
+			reader = new BufferedReader(new InputStreamReader(checkIP.openStream()));
+			return reader.readLine();
+		} catch (IOException e) {
+			logger.error("Error looking up public IP address.");
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
 	}
 }
