6c6f99b3a5d85ef2e2c2dfed4de1e6a682a8368c
Christian <Christian@DESKTOP-MVT9AEK>
2017-04-12 20:16:49 +0200

    preparations

--- monitoring_server/src/main/java/at/tuwien/monitoring/server/processing/Wsla2ExpressionMapper.java
+++ monitoring_server/src/main/java/at/tuwien/monitoring/server/processing/Wsla2ExpressionMapper.java
@@ -10,10 +10,13 @@ import javax.xml.bind.JAXBElement;
 import org.apache.log4j.Logger;
 
 import com.ibm.wsla.Equal;
+import com.ibm.wsla.FunctionType;
 import com.ibm.wsla.Greater;
 import com.ibm.wsla.GreaterEqual;
 import com.ibm.wsla.Less;
 import com.ibm.wsla.LessEqual;
+import com.ibm.wsla.Mean;
+import com.ibm.wsla.MetricType;
 import com.ibm.wsla.OperationDescriptionType;
 import com.ibm.wsla.PeriodType;
 import com.ibm.wsla.PredicateType;
@@ -37,91 +40,126 @@ public class Wsla2ExpressionMapper {
 	}
 
 	private void initInformationMap() {
-		metricMap.put("ResponseTime", new MetricInformation("ClientResponseTimeMessage", "responseTime"));
-		metricMap.put("CpuLoad", new MetricInformation("CpuLoadMessage", "cpuLoad"));
-		metricMap.put("TotalMemory", new MetricInformation("MemoryMessage", "totalMemory"));
-		metricMap.put("ResidentMemory", new MetricInformation("MemoryMessage", "residentMemory"));
+		// map of basic metrics and their corresponding messages and variables
+		metricMap.put("responsetime", new MetricInformation("ClientResponseTimeMessage", "responseTime"));
+		metricMap.put("cpuload", new MetricInformation("CpuLoadMessage", "cpuLoad"));
+		metricMap.put("totalmemory", new MetricInformation("MemoryMessage", "totalMemory"));
+		metricMap.put("residentmemory", new MetricInformation("MemoryMessage", "residentMemory"));
 	}
 
 	private void doMapping(WebServiceLevelAgreement wsla, MetricProcessor metricProcessor) {
 
 		Map<String, String> slaMetricMap = new HashMap<>();
 
+		// parse service definitions including slas and metrics
 		for (ServiceDefinitionType serviceDefinition : wsla.getWSLA().getServiceDefinition()) {
 			for (JAXBElement<? extends OperationDescriptionType> operation : serviceDefinition.getOperation()) {
 				OperationDescriptionType op = operation.getValue();
+
+				// find out all metrics for all sla parameters
 				for (SLAParameterType slaParameter : op.getSLAParameter()) {
 					slaMetricMap.put(slaParameter.getName(), slaParameter.getMetric());
+				}
+
+				// iterate metrics
+				for (MetricType metric : op.getMetric()) {
+					if (metric.getFunction() == null) {
+						// simple metric, should be in metric map if implemented
+						if (!metricMap.containsKey(metric.getName().toLowerCase())) {
+							logger.info("Metric \"" + metric.getName() + "\" is not implemented yet or not valid.");
+						}
+					} else {
+						// complex metric with function
+						parseFunction(metric.getFunction());
+					}
 				}
 			}
 		}
 
-		for (ServiceLevelObjectiveType serviceLevelObjective : wsla.getWSLA().getObligations().getServiceLevelObjective()) {
+		// parse service level objectives
+		for (ServiceLevelObjectiveType serviceLevelObjective : wsla.getWSLA().getObligations()
+				.getServiceLevelObjective()) {
 			if (!checkValidity(serviceLevelObjective.getValidity())) {
 				logger.info("Validity of SLO \"" + serviceLevelObjective.getName() + "\" not given.");
 				continue;
 			}
 
-			SimplePredicate simplePredicate = parseSimplePredicate(serviceLevelObjective.getExpression().getPredicate());
-			if (simplePredicate.getPredicateSign() == null || simplePredicate.getSlaParameter() == null
-					|| simplePredicate.getDetectionSign() == null) {
-				logger.info("Predicate of SLO \"" + serviceLevelObjective.getName() + "\" not implemented yet or not valid.");
+			SimplePredicate simplePredicate = parseSimplePredicate(
+					serviceLevelObjective.getExpression().getPredicate());
+			if (simplePredicate == null) {
+				logger.info("Predicate of SLO \"" + serviceLevelObjective.getName()
+						+ "\" not implemented yet or not valid.");
 				continue;
 			}
 
-			String metricToObserve = slaMetricMap.get(simplePredicate.getSlaParameter());
+			String metricToObserve = slaMetricMap.get(simplePredicate.getSlaParameter()).toLowerCase();
 			if (metricToObserve == null || metricMap.get(metricToObserve) == null) {
-				logger.info("SLA parameter for SLO \"" + serviceLevelObjective.getName() + "\" not defined. Ignoring SLO.");
+				logger.info(
+						"SLA parameter for SLO \"" + serviceLevelObjective.getName() + "\" not defined. Ignoring SLO.");
 				continue;
 			}
 
 			MetricInformation metricInformation = metricMap.get(metricToObserve);
 
 			String expression = String.format(DEFAULT_SIMPLE_EXPRESSION, metricInformation.getEventMessageName(),
-					metricInformation.getPropertyName(), simplePredicate.getDetectionSign(), simplePredicate.getThreshold());
+					metricInformation.getPropertyName(), simplePredicate.getDetectionSign(),
+					simplePredicate.getThreshold());
 			String requirementDesc = simplePredicate.getPredicateSign() + simplePredicate.getThreshold();
 
 			metricProcessor.addExpression(expression, requirementDesc);
 		}
 	}
 
+	private String parseFunction(FunctionType function) {
+		// currently one level and average function implemented
+		if (function instanceof Mean) {
+			Mean mean = (Mean) function;
+			return mean.getMetric();
+		}
+
+		return null;
+	}
+
 	private SimplePredicate parseSimplePredicate(PredicateType predicate) {
-
 		SimplePredicate simplePredicate = new SimplePredicate();
-
 		if (predicate instanceof Less) {
 			Less pred = (Less) predicate;
 			simplePredicate.setPredicateSign("<");
 			simplePredicate.setDetectionSign(">=");
 			simplePredicate.setSlaParameter(pred.getSLAParameter());
 			simplePredicate.setThreshold(pred.getValue());
+			return simplePredicate;
 		} else if (predicate instanceof LessEqual) {
 			LessEqual pred = (LessEqual) predicate;
 			simplePredicate.setPredicateSign("<=");
 			simplePredicate.setDetectionSign(">");
 			simplePredicate.setSlaParameter(pred.getSLAParameter());
 			simplePredicate.setThreshold(pred.getValue());
+			return simplePredicate;
 		} else if (predicate instanceof Greater) {
 			Greater pred = (Greater) predicate;
 			simplePredicate.setPredicateSign(">");
 			simplePredicate.setDetectionSign("<=");
 			simplePredicate.setSlaParameter(pred.getSLAParameter());
 			simplePredicate.setThreshold(pred.getValue());
+			return simplePredicate;
 		} else if (predicate instanceof GreaterEqual) {
 			GreaterEqual pred = (GreaterEqual) predicate;
 			simplePredicate.setPredicateSign(">=");
 			simplePredicate.setDetectionSign("<");
 			simplePredicate.setSlaParameter(pred.getSLAParameter());
 			simplePredicate.setThreshold(pred.getValue());
+			return simplePredicate;
 		} else if (predicate instanceof Equal) {
 			Equal pred = (Equal) predicate;
 			simplePredicate.setPredicateSign("=");
 			simplePredicate.setDetectionSign("!=");
 			simplePredicate.setSlaParameter(pred.getSLAParameter());
 			simplePredicate.setThreshold(pred.getValue());
+			return simplePredicate;
 		}
 
-		return simplePredicate;
+		return null;
 	}
 
 	private boolean checkValidity(List<PeriodType> validity) {
