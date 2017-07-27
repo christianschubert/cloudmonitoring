94ca3310005666237748c15d303a6e12ef35e49d
Christian <Christian@DESKTOP-MVT9AEK>
2017-07-27 13:17:13 +0200

    sucessrate to db script

--- monitoring_server/src/main/resources/createDB.sql
+++ monitoring_server/src/main/resources/createDB.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `violation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `source_ip_address` varchar(32) NOT NULL,
   `service_name` varchar(255) NOT NULL,
-  `violation_type` enum('CPU_LOAD','MEM_RESIDENT','MEM_TOTAL','RESPONSE_TIME') NOT NULL,
+  `violation_type` enum('CPU_LOAD','MEM_RESIDENT','MEM_TOTAL','RESPONSE_TIME', 'SUCCESS_RATE') NOT NULL,
   `monitored_value` float NOT NULL,
   `required_desc` varchar(255) NOT NULL,
   `violation_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
