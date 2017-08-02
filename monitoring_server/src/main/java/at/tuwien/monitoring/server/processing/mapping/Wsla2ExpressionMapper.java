f3b3ba659c6df7bcd380f3a41be61061aec3c609
Christian Schubert <christianschubert@gmx.at>
2017-08-02 15:14:55 +0200

    event processing filter values smaller zero

--- monitoring_server/src/main/java/at/tuwien/monitoring/server/processing/mapping/Wsla2ExpressionMapper.java
+++ monitoring_server/src/main/java/at/tuwien/monitoring/server/processing/mapping/Wsla2ExpressionMapper.java
@@ -35,13 +35,13 @@ public class Wsla2ExpressionMapper {
 
 	private static final int AGGREGATION_MINIMUM_EVENT_SIZE = 3;
 
-	private static final String SIMPLE_EXPRESSION = "select %s as monitoredvalue, '%s' as metrictype, '%s' as requirementdesc, * from %s having %s %s %s";
-	private static final String AGGREGATION_FUNCTION_EXPRESSION = "select %s(%s) as monitoredvalue, '%s' as metrictype, '%s' as requirementdesc, * from %s group by ipAddress having %s(%s) %s %s AND count(*) >= "
+	private static final String SIMPLE_EXPRESSION = "select %s as monitoredvalue, '%s' as metrictype, '%s' as requirementdesc, * from %s(%s > = 0) having %s %s %s";
+	private static final String AGGREGATION_FUNCTION_EXPRESSION = "select %s(%s) as monitoredvalue, '%s' as metrictype, '%s' as requirementdesc, * from %s(%s > = 0) group by ipAddress having %s(%s) %s %s AND count(*) >= "
 			+ AGGREGATION_MINIMUM_EVENT_SIZE;
 
 	private static final String RATIO_FUNCTION = "count(*, %s %s %s)/count(*)";
 	private static final String RATIO_EXPRESSION = "select " + RATIO_FUNCTION
-			+ " as monitoredvalue, '%s' as metrictype, '%s' as requirementdesc, * from %s group by ipAddress having "
+			+ " as monitoredvalue, '%s' as metrictype, '%s' as requirementdesc, * from %s(%s > = 0) group by ipAddress having "
 			+ RATIO_FUNCTION + " %s %s AND count(*) >=" + AGGREGATION_MINIMUM_EVENT_SIZE;
 
 	@SuppressWarnings("unused")
@@ -134,8 +134,8 @@ public class Wsla2ExpressionMapper {
 			String successPattern = "'2%'";
 			expression = String.format(RATIO_EXPRESSION, metricInformation.getPropertyName(), "like", successPattern,
 					metricInformation.getPropertyName(), requirementDesc, metricInformation.getEventMessageName(),
-					metricInformation.getPropertyName(), "like", successPattern, simplePredicate.getDetectionSign(),
-					simplePredicate.getThreshold());
+					metricInformation.getPropertyName(), metricInformation.getPropertyName(), "like", successPattern,
+					simplePredicate.getDetectionSign(), simplePredicate.getThreshold());
 
 		} else if (metricInformation.getAggregationFunction() == null) {
 			// simple function
@@ -143,7 +143,8 @@ public class Wsla2ExpressionMapper {
 
 			expression = String.format(SIMPLE_EXPRESSION, metricInformation.getPropertyName(),
 					metricInformation.getPropertyName(), requirementDesc, metricInformation.getEventMessageName(),
-					metricInformation.getPropertyName(), simplePredicate.getDetectionSign(), simplePredicate.getThreshold());
+					metricInformation.getPropertyName(), metricInformation.getPropertyName(), simplePredicate.getDetectionSign(),
+					simplePredicate.getThreshold());
 
 		} else {
 			// aggregation function
@@ -152,8 +153,9 @@ public class Wsla2ExpressionMapper {
 
 			expression = String.format(AGGREGATION_FUNCTION_EXPRESSION, metricInformation.getAggregationFunction(),
 					metricInformation.getPropertyName(), metricInformation.getPropertyName(), requirementDesc,
-					metricInformation.getEventMessageName(), metricInformation.getAggregationFunction(),
-					metricInformation.getPropertyName(), simplePredicate.getDetectionSign(), simplePredicate.getThreshold());
+					metricInformation.getEventMessageName(), metricInformation.getPropertyName(),
+					metricInformation.getAggregationFunction(), metricInformation.getPropertyName(),
+					simplePredicate.getDetectionSign(), simplePredicate.getThreshold());
 		}
 
 		metricProcessor.addExpression(expression);
