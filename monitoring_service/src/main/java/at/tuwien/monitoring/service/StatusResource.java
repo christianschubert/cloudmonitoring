32810a692598f1ab33c23a94c818e386925eba31
Christian <Christian@DESKTOP-MVT9AEK>
2017-08-24 15:42:32 +0200

    status fix

--- monitoring_service/src/main/java/at/tuwien/monitoring/service/StatusResource.java
+++ monitoring_service/src/main/java/at/tuwien/monitoring/service/StatusResource.java
@@ -4,13 +4,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import at.tuwien.monitoring.service.message.Message;
 
 @Path("status")
 public class StatusResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getStatus() {
-		return "Online";
+	public Response getStatus() {
+		return Response.status(Response.Status.OK).entity(new Message("Online")).build();
 	}
 }
