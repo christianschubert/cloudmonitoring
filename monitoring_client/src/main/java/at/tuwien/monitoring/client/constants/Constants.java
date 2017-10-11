0d1ff23afa17519a05dfb4bbbeaf5043645e6ca2
Christian Schubert <christianschubert@gmx.at>
2017-10-11 13:53:06 +0200

    new images, rename resizer to processor

--- monitoring_client/src/main/java/at/tuwien/monitoring/client/constants/Constants.java
+++ monitoring_client/src/main/java/at/tuwien/monitoring/client/constants/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	private Constants() {
 	}
 
-	public static final String APP_PATH = "/imageresizer/shrink";
+	public static final String APP_PATH = "/imageProcessor/shrink";
 
 	public static final String UPLOAD_PATH = "upload/";
 	public static final String DOWNLOAD_PATH = "download/";
